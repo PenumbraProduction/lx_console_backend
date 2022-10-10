0a207a207e2db44c18a9e0777a7d49c16f97568f
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2022-10-10 09:28:38 +0100

    Add Function and Uncategorised properties for palettes

--- src/models/Desk.ts
+++ src/models/Desk.ts
@@ -1,19 +1,21 @@
 import { PatchManager } from "./channels/PatchManager";
 import { PlaybackManager } from "./playbacks/PlaybackManager";
-import { BeamPaletteItem, ColourPaletteItem, CuePaletteItem, GenericPaletteItem, GroupPaletteItem, Palette, PositionPaletteItem, ShapePaletteItem } from "./palettes";
+import { CuePaletteItem, GenericPaletteItem, GroupPaletteItem, Palette } from "./palettes";
 
 export class Desk {
 	patch: PatchManager;
 	
 	groups: Palette<GroupPaletteItem>;
 	
-	cues: Palette<CuePaletteItem>;
+	cues: Palette<GenericPaletteItem>;
 	playbacks: PlaybackManager;
 
 	colour: Palette<GenericPaletteItem>;
 	beam: Palette<GenericPaletteItem>;
 	shape: Palette<GenericPaletteItem>;
 	position: Palette<GenericPaletteItem>;
+	function: Palette<GenericPaletteItem>;
+	uncategorised: Palette<GenericPaletteItem>;
 	
 	constructor() {
 		this.patch = new PatchManager();
@@ -22,9 +24,11 @@ export class Desk {
 		this.beam = new Palette<GenericPaletteItem>("Beam #");
 		this.shape = new Palette<GenericPaletteItem>("Shape #");
 		this.position = new Palette<GenericPaletteItem>("Position #");
+		this.function = new Palette<GenericPaletteItem>("Function #");
+		this.uncategorised = new Palette<GenericPaletteItem>("Uncategorised #");
 
 		this.groups = new Palette<GroupPaletteItem>("Group #");
-		this.cues = new Palette<CuePaletteItem>("Cue #");
+		this.cues = new Palette<GenericPaletteItem>("Cue #");
 
 		this.playbacks = new PlaybackManager();
 	}
