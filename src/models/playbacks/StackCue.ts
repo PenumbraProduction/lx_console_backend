76442624468703dea442d0b5b9177d13769bc853
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2022-10-12 14:26:07 +0100

    specify cue source type and data

--- src/models/playbacks/StackCue.ts
+++ src/models/playbacks/StackCue.ts
@@ -3,22 +3,21 @@ import { CuePaletteItem } from "../palettes";
 import { Transition } from "../Transition";
 
 export class StackCue {
-	source: number | Map<ChannelAddress, number>;
+	source: StackCueSource;
 	id: string;
 	name: string;
-	cueTransitions: Map<string, {cue: CuePaletteItem, transition: Transition}>;
+	cueTransitions: Map<string, { cue: CuePaletteItem; transition: Transition }>;
 
-
-	constructor(id: string, name: string, source: number | Map<ChannelAddress, number>) {
+	constructor(id: string, name: string, source: StackCueSource) {
 		this.source = source;
 		this.id = id;
 		this.name = name;
 
-		this.generateTransitionGroups()
+		this.generateTransitionGroups();
 	}
 
-	private generateTransitionGroups() {
-		
-	}
+	private generateTransitionGroups() {}
 }
 
+export type StackCueSourceType = "cue" | "raw";
+export type StackCueSource = { type: StackCueSourceType; content: number | Map<ChannelAddress, number> };
